4062c2449ea19a59b8f76f420dd08f6142ed9343
HuyHieu <huyhieu.hust2855@gmail.com>
2023-10-09 13:09:22 +0700

    Add project files.

---
+++ C#Basic/DateTimeAddHours.cs
@@ -0,0 +1,16 @@
+﻿namespace C_Basic
+{
+    internal class DateTimeAddHours
+    {
+
+        public static void DateTimeAddHours1()
+        {
+            double[] hours = {.08333, .16667, .25, .33333, .5, .66667, 1, 2,
+29, 30, 31, 90, 365};
+            DateTime dateValue = new DateTime(2009, 3, 1, 12, 0, 0);
+            foreach (double hour in hours)
+                Console.WriteLine("{0} + {1} hour(s) = {2}", dateValue, hour,
+                dateValue.AddHours(hour));
+        }
+    }
+}
---
+++ C#Basic/Program.cs
@@ -0,0 +1,8 @@
+﻿using C_Basic;
+
+// Section 19.2: DateTime.AddDays(Double)
+DateTimeAddDays.DateTimeAddDays1();
+DateTimeAddDays.DateTimeAddDays2();
+
+// Section 19.3: DateTime.AddHours(Double
+DateTimeAddHours.DateTimeAddHours1();
